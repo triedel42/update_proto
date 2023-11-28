c4f760d37d23b4f7055ac86e89a3aea34f066bf3
Tobias Riedel <triedel@student.42wolfsburg.de>
2023-11-28 11:56:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# update_proto.py
+A quick script to update your C prototypes using cproto.
+
+Use at your own risk ;)
+
+## Installation
+- Install [homebrew](https://docs.brew.sh/Installation#untar-anywhere-unsupported)
+- Install [cproto](https://invisible-island.net/cproto/cproto.html) with `$ brew install cproto`
+
+---
+
+If you want to install it to your home directory:
+```
+$ cd
+$ git clone https://github.com/triedel42/update_proto
+```
+
+then add this to your `~/.zshrc`
+
+```bash
+export PATH="$PATH:$HOME/update_proto
+```
+
+## Usage
+Add this to your header file where you want your prototypes to be.
+
+```C
+/* $$proto_start$$ */
+
+// this will be replaced and updated
+// by update_proto.py
+
+/* $$proto_end$$ */
+```
+
+Now you can update myfile.h using auto-generated prototypes from myfile.c.
+```bash
+$ update_proto.py myfile.h
+```
+
+---
+
+You can also use `$ update_proto.py *.h` or add a rule with it to your Makefile. `update_proto.py` accepts multiple arguments like `$ update_proto.py myfile1.h myfile2.h myfile3.h`.
---
+++ update_proto.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from sys import argv
+import subprocess
+import getpass
+
+
+bin_cproto = '/Users/' + getpass.getuser() + '/homebrew/bin/cproto'
+
+
+def update_proto(header) -> bool:
+    cfile = header.replace('.h', '.c')
+    did_something = False
+    inside_proto = False
+    lines_new = []
+    for line in open(header).read().splitlines(True):
+        if '$$proto_end$$' in line:
+            if inside_proto:
+                lines_new.append('\n')
+            inside_proto = False
+        if not inside_proto:
+            lines_new.append(line)
+        if '$$proto_start$$' in line:
+            lines_new.append('\n')
+            if not inside_proto:
+                lines_new.extend(subprocess.run(f'{bin_cproto} {cfile}'.split(' '),
+                        capture_output=True, text=True).stdout.splitlines(True))
+            inside_proto = True
+            did_something = True
+
+    assert not inside_proto
+    open(header, 'w').write(''.join(lines_new))
+    return did_something
+
+
+if __name__ == '__main__':
+    assert len(argv) >= 2
+
+    for header in argv[1:]:
+        assert header.endswith('.h')
+        res = update_proto(header)
+        msg = 'Updated ' + header
+        msg += ' (No $$proto_start$$ and $$proto_end$$)' if res else ''
+        print(msg)
